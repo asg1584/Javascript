76be07f3d9f3bf8286304bd97ba0bdb14988975d
asg1584 <86628659+asg1584@users.noreply.github.com>
2022-01-20 14:02:41 +0100

    Delete 4.hafta directory

--- 4.hafta/js/app.js
+++
@@ -1,8 +0,0 @@
-//Pilin seviye bilgisini tutmak icin bu degisken tanimlandi
-let seviye = 0;
-
-//Doldur butonuna tiklandiginda  "functionDoldur" fonsiyonu cagiriliyor yani pilin seviyesi artiriliyor
-doldurBtn.addEventListener("click", functionDoldur);
-
-//Bosalt butonuna tiklandiginda  "functionBosalt" fonsiyonu cagiriliyor yani pilin seviyesi azaltiliyor
-bosaltBtn.addEventListener("click", functionBosalt);
--- 4.hafta/js/dom.js
+++
@@ -1,6 +0,0 @@
-const bar1 = document.querySelector("#bar1");
-const bar2 = document.querySelector("#bar2");
-const bar3 = document.querySelector("#bar3");
-const doldurBtn = document.querySelector("#doldur");
-const bosaltBtn = document.querySelector("#bosalt");
-const seviyeLabel = document.querySelector("#seviye");
